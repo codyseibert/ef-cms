567ac2e18eadeca5e9e924084673016965514f22
Matt Hopson <matthopson@gmail.com>
2021-04-06 08:58:54 -0600

    DevEx 687: WIP - Test sequence state mutations

---
+++ web-client/src/presenter/sequences/updateDocketEntryFormValueSequence.test.js
@@ -0,0 +1,253 @@
+import { CerebralTest } from 'cerebral/test';
+import { applicationContextForClient as applicationContext } from '../../../../shared/src/business/test/createTestApplicationContext';
+import { presenter } from '../presenter';
+// import { updateDocketEntryFormValueSequence } from './updateDocketEntryFormValueSequence';
+
+let test;
+let mockCaseDetail;
+let mockFormState;
+
+describe('updateDocketEntryFormValueSequence', () => {
+  beforeAll(() => {
+    presenter.providers.applicationContext = applicationContext;
+    test = CerebralTest(presenter);
+
+    mockCaseDetail = {
+      docketEntries: [
+        {
+          docketEntryId: '123',
+          documentTitle: 'Test Document One',
+        },
+        {
+          docketEntryId: '234',
+          documentTitle: 'Test Document Two',
+        },
+        {
+          docketEntryId: '345',
+          documentTitle: 'Test Document Three',
+        },
+      ],
+    };
+
+    mockFormState = {
+      category: 'beep',
+      certificateOfServiceDate: 'some-date',
+      certificateOfServiceDay: 'some-day',
+      certificateOfServiceMonth: 'some-month',
+      certificateOfServiceYear: 'some-year',
+      documentTitle: 'Some Random Document Title',
+      documentType: 'boop',
+      scenario: 'bop',
+    };
+  });
+
+  beforeEach(() => {
+    test.setState('caseDetail', mockCaseDetail);
+    test.setState('form', { ...mockFormState });
+  });
+
+  describe('any key', () => {
+    it('sets the props.value on state.form', async () => {
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'someKey',
+        value: 'someValue',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        someKey: 'someValue',
+      });
+    });
+
+    it('unsets the state.form property with the name given in props.key if the props.value is an empty string', async () => {
+      test.setState('form.someKey', 'someValue');
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        someKey: 'someValue',
+      });
+
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'someKey',
+        value: '',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        someKey: undefined,
+      });
+
+      expect(test.getState('form.someKey')).toBeUndefined();
+    });
+  });
+
+  describe('previousDocument', () => {
+    it('sets the previousDocument from the case docketEntries on state.form.previousDocument', async () => {
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'previousDocument',
+        value: mockCaseDetail.docketEntries[1].docketEntryId,
+      });
+
+      expect(test.getState('form.previousDocument')).toEqual(
+        mockCaseDetail.docketEntries[1],
+      );
+      expect(
+        test.getState('form.secondaryDocument.previousDocument'),
+      ).toBeUndefined();
+    });
+  });
+
+  describe('secondaryDocument.previousDocument', () => {
+    it('sets the secondaryDocument.previousDocument from the case docketEntries on state.form.secondaryDocument.previousDocument', async () => {
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'secondaryDocument.previousDocument',
+        value: mockCaseDetail.docketEntries[1].docketEntryId,
+      });
+
+      expect(test.getState('form.secondaryDocument.previousDocument')).toEqual(
+        mockCaseDetail.docketEntries[1],
+      );
+      expect(test.getState('form.previousDocument')).toBeUndefined();
+    });
+  });
+
+  describe('initEventCode', () => {
+    it('overwrites form values for `category`, `documentType`, and `scenario` with those respective values from the INTERNAL_EVENT_CATEGORY entry with the given eventCode', async () => {
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'initEventCode',
+        value: 'A',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        category: 'Answer (filed by respondent only)',
+        // documentTitle does not change
+        documentType: 'Answer',
+        initEventCode: 'A',
+        scenario: 'Standard',
+      });
+    });
+  });
+
+  describe('initEventCode', () => {
+    it('unsets certificateOfService date part fields when the the props.key is `certificateOfService`', async () => {
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'certificateOfService',
+        value: true,
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        certificateOfService: true,
+        certificateOfServiceDate: undefined,
+        certificateOfServiceDay: undefined,
+        certificateOfServiceMonth: undefined,
+        certificateOfServiceYear: undefined,
+      });
+    });
+  });
+
+  describe('eventCode', () => {
+    it('overwrites form values for `category`, `documentTitle`, `documentType`, and `scenario` with those respective values from the INTERNAL_EVENT_CATEGORY entry with the given eventCode', async () => {
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'eventCode',
+        value: 'ACED',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        category: 'Decision',
+        documentTitle: 'Agreed Computation for Entry of Decision',
+        documentType: 'Agreed Computation for Entry of Decision',
+        eventCode: 'ACED',
+        scenario: 'Standard',
+      });
+    });
+
+    it('unsets form.previousDocument when the props.key is eventCode and state.screenMetadata.supporting is falsy', async () => {
+      test.setState('form.previousDocument', {});
+      test.setState('screenMetadata.supporting', undefined);
+
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'eventCode',
+        value: 'ACED',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        category: 'Decision',
+        documentTitle: 'Agreed Computation for Entry of Decision',
+        documentType: 'Agreed Computation for Entry of Decision',
+        eventCode: 'ACED',
+        previousDocument: undefined,
+        scenario: 'Standard',
+      });
+    });
+
+    it('sets form.previousDocument and merges screenMetadata.primary when the props.key is eventCode and there is one docketEntryId in screenMetadata.filedDocketEntryIds matching an entry in the currentCaseDetail.docketEntries', async () => {
+      test.setState('form.previousDocument', {});
+      test.setState('screenMetadata.supporting', true);
+      test.setState('screenMetadata.filedDocketEntryIds', ['234']);
+      test.setState('screenMetadata.primary', { foo: 'bar' });
+
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'eventCode',
+        value: 'ACED',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        category: 'Decision',
+        documentTitle: 'Agreed Computation for Entry of Decision',
+        documentType: 'Agreed Computation for Entry of Decision',
+        eventCode: 'ACED',
+        foo: 'bar', // from screenMetadata.primary
+        previousDocument: mockCaseDetail.docketEntries[1],
+        scenario: 'Standard',
+      });
+    });
+
+    it('does not set form.previousDocument or merge screenMetadata.primary when the props.key is eventCode and there is more than one docketEntryId in screenMetadata.filedDocketEntryIds', async () => {
+      test.setState('form.previousDocument', {});
+      test.setState('screenMetadata.supporting', true);
+      test.setState('screenMetadata.filedDocketEntryIds', ['123', '234']);
+      test.setState('screenMetadata.primary', { foo: 'bar' });
+
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'eventCode',
+        value: 'ACED',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        category: 'Decision',
+        documentTitle: 'Agreed Computation for Entry of Decision',
+        documentType: 'Agreed Computation for Entry of Decision',
+        eventCode: 'ACED',
+        previousDocument: {},
+        scenario: 'Standard',
+      });
+    });
+
+    it('does not set form.previousDocument or merge screenMetadata.primary when the props.key is eventCode and there is one docketEntryId in screenMetadata.filedDocketEntryIds that does not match an entry in the currentCaseDetail.docketEntries', async () => {
+      test.setState('form.previousDocument', {});
+      test.setState('screenMetadata.supporting', true);
+      test.setState('screenMetadata.filedDocketEntryIds', ['nope']);
+      test.setState('screenMetadata.primary', { foo: 'bar' });
+
+      await test.runSequence('updateDocketEntryFormValueSequence', {
+        key: 'eventCode',
+        value: 'ACED',
+      });
+
+      expect(test.getState('form')).toEqual({
+        ...mockFormState,
+        category: 'Decision',
+        documentTitle: 'Agreed Computation for Entry of Decision',
+        documentType: 'Agreed Computation for Entry of Decision',
+        eventCode: 'ACED',
+        previousDocument: {},
+        scenario: 'Standard',
+      });
+    });
+  });
+});
