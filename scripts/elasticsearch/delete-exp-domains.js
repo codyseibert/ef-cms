648ce9e601d93e804da80c8f182ecdf28c27433d
Cody Seibert <cseibert113@gmail.com>
2021-12-13 11:05:04 -0500

    adding a script we can use to delete unused exp clusters

---
+++ scripts/elasticsearch/delete-exp-domains.js
@@ -0,0 +1,18 @@
+const AWS = require('aws-sdk');
+const es = new AWS.ES({ region: 'us-east-1' });
+
+(async () => {
+  const { DomainNames } = await es.listDomainNames().promise();
+
+  const expDomains = DomainNames.filter(domain =>
+    domain.DomainName.includes('-exp'),
+  );
+
+  for (let domain of expDomains) {
+    await es
+      .deleteElasticsearchDomain({ DomainName: domain.DomainName })
+      .promise();
+    console.log('deleting domain: ', domain.DomainName);
+    await new Promise(resolve => setTimeout(resolve, 60000));
+  }
+})();
